f9fcdf149600e8219e58e1506f8a7e352e9b8b95
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2024-02-07 21:14:50 +0100

    Switch to axios for http requests

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,23 +1,27 @@
-const baseUrl = "https://grade-planner-be.onrender.com/api/v1/";
+import axios, { type AxiosProgressEvent } from "axios";
 
-export function sendFile(endpoint: string ,file: File) {
-  let formdata = new FormData();
-  formdata.append("file", file);
-  
-  return fetch(baseUrl + endpoint, {
-    method: "POST",
-    body: formdata
-  });
+const baseURL = "https://grade-planner-be.onrender.com/api/v1/";
+const control = new AbortController();
+const signal = control.signal;
+
+const instance = axios.create({
+  baseURL,
+});
+
+export const controller = control;
+
+export function sendFile(endpoint: string ,file: File, upload?: ((e: AxiosProgressEvent) => void), download? : ((e: AxiosProgressEvent) => void)) {
+  return instance.post(endpoint, {
+    signal,
+    file
+  }, {
+    onUploadProgress: upload,
+    onDownloadProgress: download,
+    headers: {
+      'Content-Type': "multipart/form-data"
+    }
+  })
 }
 
 export function sendJson(endpoint: string) {
-  return fetch(baseUrl + endpoint, {
-    method: "POST",
-    
-  });
-}
-
-export function printUrl() {
-
-  console.log(baseUrl);
-}
+  }
