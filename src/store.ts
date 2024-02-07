ee259dd39b4afb976e68b98f31efb7dbabad0dfe
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2024-02-07 21:16:07 +0100

    Create global state

---
+++ src/store.ts
@@ -0,0 +1,52 @@
+import {reactive} from "vue";
+
+interface CourseResult {
+	course_code: string,
+	title: string,
+	unit: number,
+	grade: string,
+	grade_point: number
+}
+
+interface SemesterResult {
+	courses: Array<CourseResult>,
+	gpa: number,
+	title: string,
+	total_grade_points: number,
+	total_units: number
+}
+
+interface CourseStructure {
+	course_code: string,
+	title: string,
+	unit: number,
+	is_elective: boolean
+}
+
+interface SemesterStructure {
+	courses: Array<CourseStructure>,
+	title: string
+}
+
+export const store = reactive({
+	studentCGPA: sessionStorage.getItem("cgpa") ? Number(sessionStorage.getItem("cgpa")) : 0,
+	numberOfUnits: sessionStorage.getItem("units") ? Number(sessionStorage.getItem("units")) : 0,
+	semesterResults: sessionStorage.getItem("sem_results") ? JSON.parse(sessionStorage.getItem("sem_results")!).semesters : [<SemesterResult>{}],
+	courseStructure: sessionStorage.getItem("structure") ? JSON.parse(sessionStorage.getItem("structure")!).semesters : [<SemesterStructure>{}],
+	changeCGPA(cgpa: number) {
+		this.studentCGPA = cgpa;
+		sessionStorage.setItem("cgpa", this.studentCGPA.toString());
+	},
+	changeTotalUnits(units: number) {
+		this.numberOfUnits = units;
+		sessionStorage.setItem("units", this.numberOfUnits.toString());
+	},
+	updateSemesterResults(sems: Array<SemesterResult>) {
+		this.semesterResults = sems;
+		sessionStorage.setItem("sem_results", JSON.stringify({"semesters": this.semesterResults}));
+	},
+	updateCourseStructure(courses: Array<SemesterStructure>) {
+		this.courseStructure = courses;
+		sessionStorage.setItem("structure", JSON.stringify({"semesters": this.semesterResults}));
+	}
+});
