39ca3907f2f980655aaa59db1781760701643909
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2024-02-10 16:33:16 +0100

    Make Generate Grades view responsive

--- src/components/GradeTable.vue
+++ src/components/GradeTable.vue
@@ -4,132 +4,220 @@ import { sendJson } from "@/fetch";
 import { reactive, ref, onMounted } from "vue";
 
 interface ResponseJSON {
-	course_code: string,
-	title: string,
-	unit: number,
-	grade_int: number,
-	grade: string
+  course_code: string;
+  title: string;
+  unit: number;
+  grade_int: number;
+  grade: string;
 }
 
 const emit = defineEmits(["gradeChange"]);
 
 const props = defineProps<{
-	title: string,
-	courses: Array<CourseStructure>,
+  title: string;
+  courses: Array<CourseStructure>;
 }>();
 
-let dropdownName = ref((props.title.split(' ').join('')));
+let dropdownName = ref(props.title.split(" ").join(""));
 let coursesArray = reactive(props.courses);
 let generated_grades = reactive<Array<ResponseJSON>>([<ResponseJSON>{}]);
 let totalUnits = ref(0);
 let gpa = ref(0);
 
 onMounted(() => {
-	let totalSemesterUnits: number = 0;
+  let totalSemesterUnits: number = 0;
 
-	coursesArray.forEach((course) => {
-		totalSemesterUnits += course.unit;
-	});
+  coursesArray.forEach((course) => {
+    totalSemesterUnits += course.unit;
+  });
 
-	totalUnits.value = totalSemesterUnits;
-	regenerateHandler();
+  totalUnits.value = totalSemesterUnits;
+  regenerateHandler();
 });
 
 function regenerateHandler() {
-	let response = sendJson("generate_grades", coursesArray, 5.0);
-	response.then(res => {
-		generated_grades = res.data.data.patterns[0].generated_grades;
-		gpa.value = res.data.data.generated_gpa;
-	});
+  let response = sendJson("generate_grades", coursesArray, 5);
+  response.then((res) => {
+    generated_grades = res.data.data.patterns[0].generated_grades;
+    gpa.value = res.data.data.generated_gpa;
+  });
 }
 
 function changeGrade(e: Event, index: number) {
-	let totalGradePoints = 0;
-	let dropdowns = document.getElementsByName(dropdownName.value);
+  let totalGradePoints = 0;
+  let dropdowns = document.getElementsByName(dropdownName.value);
 
-	generated_grades[index].grade_int = Number((e.target as HTMLInputElement).value);
-	generated_grades[index].grade = (document.getElementsByTagName("option")[(e.target as HTMLSelectElement).selectedIndex] as HTMLOptionElement).textContent!;
+  generated_grades[index].grade_int = Number(
+    (e.target as HTMLInputElement).value,
+  );
+  generated_grades[index].grade = (
+    document.getElementsByTagName("option")[
+      (e.target as HTMLSelectElement).selectedIndex
+    ] as HTMLOptionElement
+  ).textContent!;
 
-	dropdowns.forEach((dropdown, i) => {
-		if (generated_grades[i]) {
-			totalGradePoints += Number((dropdown as HTMLInputElement).value) * generated_grades[i].unit;
-		}
-	});
+  dropdowns.forEach((dropdown, i) => {
+    if (generated_grades[i]) {
+      totalGradePoints +=
+        Number((dropdown as HTMLInputElement).value) * generated_grades[i].unit;
+    }
+  });
 
-	console.log(dropdowns);
-	console.log(generated_grades);
-	gpa.value = totalGradePoints / totalUnits.value;
+  gpa.value = totalGradePoints / totalUnits.value;
 
-	console.log(gpa.value);
-	emit("gradeChange", totalGradePoints, totalUnits.value);
+  emit("gradeChange", totalGradePoints, totalUnits.value);
 }
-
 </script>
 
 <template>
-	<div class="table">
-		<div class="container">
-			<header>
-				<h1>{{ props.title }}</h1>
-				<button @click="regenerateHandler">Regenerate</button>
-			</header>
-			<div>
-				<span>Course Code</span>
-				<span>Units</span>
-				<span>Grade</span>
-			</div>
-			<div v-for="(course, i) in generated_grades" :key="course.course_code">
-				<span>{{ course.course_code + "\t" }} </span>
-				<span>{{ course.unit }}</span>
-				<span>
-					<select :name="dropdownName" :id="dropdownName" :value="course.grade_int"
-						@change="(e) => changeGrade(e, i)">
-						<option value="5">A</option>
-						<option value="4">B</option>
-						<option value="3">C</option>
-						<option value="1">D</option>
-						<option value="0">F</option>
-					</select>
-				</span>
-			</div>
-			<p>GPA: <span>{{ gpa.toFixed(2) }}</span></p>
-			<p>Number of Units: <span>{{ totalUnits }}</span></p>
-		</div>
-	</div>
+  <div class="container">
+    <header>
+      <h1>{{ props.title }}</h1>
+      <button type="button" @click="regenerateHandler">Regenerate</button>
+    </header>
+    <div class="table">
+      <span class="table-header">Course Code</span>
+      <span class="table-header">Units</span>
+      <span class="table-header">Grade</span>
+      <span class="row-separator"></span>
+      <template
+        v-for="(course, i) in generated_grades"
+        :key="course.course_code"
+      >
+        <span>{{ course.course_code + "\t" }} </span>
+        <span>{{ course.unit }}</span>
+        <span>
+          <select
+            :name="dropdownName"
+            :id="dropdownName"
+            :value="course.grade_int"
+            @change="(e) => changeGrade(e, i)"
+          >
+            <option value="5">A</option>
+            <option value="4">B</option>
+            <option value="3">C</option>
+            <option value="2">D</option>
+            <option value="0">F</option>
+          </select>
+        </span>
+        <span class="row-separator"></span>
+      </template>
+    </div>
+    <p>
+      GPA: <span>{{ gpa.toFixed(2) }}</span>
+    </p>
+    <p>
+      Number of Units: <span>{{ totalUnits }}</span>
+    </p>
+  </div>
 </template>
 
 <style scoped>
 .container {
-	border: 2px solid var(--color-border);
-	border-radius: 16px;
-	background-color: var(--color-background-tile);
-	color: var(--color-text-4);
-	padding: 20px;
-	display: grid;
-	margin-bottom: 20px;
-	@apply grid-rows-3
+  border: 1px solid var(--color-border);
+  border-radius: 16px;
+  background-color: var(--color-background-tile);
+  color: var(--color-text-4);
+  padding: 20px;
+  margin-bottom: 20px;
 }
 
-.table {}
+.table {
+  display: grid;
+  text-align: center;
+  align-items: center;
+  justify-content: space-between;
+  justify-items: center;
+  margin-bottom: 20px;
+  @apply grid-cols-3 gap-y-3;
+}
 
-.table th td {
-	color: var(--color-text-blue);
+.table-header {
+  color: var(--color-text-blue);
+  font-weight: 500;
+}
+
+.table span {
+  font-size: 14px;
+}
+
+.table .row-separator {
+  grid-column: 1 / -1;
+  border: 1px solid #f1f5f9;
+  width: 100%;
+  margin: 0;
+}
+
+header {
+  margin-bottom: 24px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 }
 
 header h1 {
-	color: var(--color-text-blue);
-	display: inline;
+  color: var(--color-text-blue);
+  font-weight: 500;
+  font-size: 16px;
 }
 
 header button {
-	background-color: var(--color-text-blue);
-	color: #fff;
-	display: inline;
+  background-color: var(--color-text-blue);
+  color: #fff;
+  padding: 6px 15px;
+  border-radius: 32px;
+  width: auto;
+  align-self: flex-end;
 }
 
 select {
-	border: 2px solid var(--color-border);
-	border-radius: 4px;
-	background-color: var(--color-background-tile);
+  border: 1px solid #f1f5f9;
+  padding: 2px;
+  border-radius: 4px;
+  background-color: var(--color-background-tile);
+}
+
+.container > p {
+  margin-top: 8px;
+  text-align: right;
+  font-size: 14px;
+}
+
+.container > p > span {
+  color: var(--color-text-blue);
+  font-weight: 500;
+}
+
+@media (min-width: 640px) {
+  .container {
+    width: 40%;
+    padding: 24px;
+  }
+
+  header {
+    text-align: left;
+    padding: 0 15px;
+    margin-bottom: 32px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    align-content: center;
+  }
+
+  header h1 {
+    font-size: 18px;
+  }
+
+  header button {
+    margin-right: 0;
+  }
+
+  .table span {
+    font-size: 16px;
+  }
+}
+
+@media (min-width: 1024px) {
 }
 </style>
--- src/views/GenerateGradesView.vue
+++ src/views/GenerateGradesView.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import IconArrowLeft from '@/components/icons/IconArrowLeft.vue';
-import GradeTable from '@/components/GradeTable.vue';
-import { store, type SemesterStructure } from '@/store';
-import { onMounted, reactive, ref } from 'vue';
+import IconArrowLeft from "@/components/icons/IconArrowLeft.vue";
+import GradeTable from "@/components/GradeTable.vue";
+import { store, type SemesterStructure } from "@/store";
+import { onMounted, reactive, ref } from "vue";
 
 interface Result {
-	id: number,
-	courses?: Array<Object>,
-	title: string,
-	total_units: number,
-	total_grade_points: number,
-	gpa?: number
+  id: number;
+  courses?: Array<Object>;
+  title: string;
+  total_units: number;
+  total_grade_points: number;
+  gpa?: number;
 }
 
 let studentCGPA = ref(0);
@@ -19,89 +19,134 @@ let studentUnits = ref(0);
 let results = reactive<Array<Result>>([]);
 
 onMounted(() => {
-	let grade_points = 0;
-	let units = 0;
+  let grade_points = 0;
+  let units = 0;
 
-	console.log(store);
+  store.courseStructure.forEach((course: SemesterStructure, i: number) => {
+    if (!course.estimate) {
+      grade_points += Number(store.semesterResults[i].total_grade_points);
+      units += Number(store.semesterResults[i].total_units);
+    }
+  });
 
-	store.courseStructure.forEach((course: SemesterStructure, i: number) => {
-		if (!course.estimate) {
-			grade_points += Number(store.semesterResults[i].total_grade_points);
-			units += Number(store.semesterResults[i].total_units);
-		}
-	});
+  // store.semesterResults.forEach((semester: Result) => {
+  // 	results[index].id = index;
+  // 	results[index].title = semester.title;
+  // 	results[index].total_units = semester.total_units;
+  // 	results[index].total_grade_points = semester.total_grade_points;
+  // 	resultsIndex.value += 1;
+  // });
 
-	// store.semesterResults.forEach((semester: Result) => {
-	// 	results[index].id = index;
-	// 	results[index].title = semester.title;
-	// 	results[index].total_units = semester.total_units;
-	// 	results[index].total_grade_points = semester.total_grade_points;
-	// 	resultsIndex.value += 1;
-	// });
-
-	studentCGPA.value = Number((grade_points / units).toFixed(2));
-	studentGradePoints.value = grade_points;
-	studentUnits.value = units;
+  studentCGPA.value = Number((grade_points / units).toFixed(2));
+  studentGradePoints.value = grade_points;
+  studentUnits.value = units;
 });
 
 function calculateCGPA(totalGradePoints: number, totalUnits: number) {
-	let grade_points: number = totalGradePoints + studentGradePoints.value;
-	let units: number = totalUnits + studentUnits.value;
+  let grade_points: number = totalGradePoints + studentGradePoints.value;
+  let units: number = totalUnits + studentUnits.value;
 
-	studentCGPA.value = Number((grade_points / units).toFixed(2));
+  studentCGPA.value = Number((grade_points / units).toFixed(2));
 }
-
 </script>
 
 <template>
-	<main>
-		<RouterLink to="/upload-course" class="step-info">
-			<IconArrowLeft class="icon" />
-			<span>Step 3/3</span>
-		</RouterLink>
-		<div class="grade-data">
-			<p>Estimated CGPA: <span>{{ studentCGPA }}</span></p>
-			<p>Total Number of Units: <span>{{ store.numberOfUnits }}</span></p>
-			<p>Current CGPA: <span>{{ store.studentCGPA }}</span></p>
-		</div>
-		<div class="tables">
-			<div v-for="semester in store.courseStructure" :key="semester.id">
-				<GradeTable :title="semester.title" :courses="semester.courses" v-if="semester.estimate"
-					@grade-change="(totalGradePoints, totalUnits) => calculateCGPA(totalGradePoints, totalUnits)" />
-			</div>
-		</div>
-	</main>
+  <main>
+    <RouterLink to="/upload-course" class="step-info">
+      <IconArrowLeft class="icon" />
+      <span>Step 3/3</span>
+    </RouterLink>
+    <div class="grade-data">
+      <p>
+        Estimated CGPA: <span>{{ studentCGPA }}</span>
+      </p>
+      <p>
+        Total Number of Units: <span>{{ store.numberOfUnits }}</span>
+      </p>
+      <p>
+        Current CGPA: <span>{{ store.studentCGPA }}</span>
+      </p>
+    </div>
+    <div class="tables">
+      <template v-for="semester in store.courseStructure" :key="semester.id">
+        <GradeTable
+          :title="semester.title"
+          :courses="semester.courses"
+          v-if="semester.estimate"
+          @grade-change="
+            (totalGradePoints, totalUnits) =>
+              calculateCGPA(totalGradePoints, totalUnits)
+          "
+        />
+      </template>
+    </div>
+  </main>
 </template>
 
 <style scoped>
 main {
-	margin: 20px;
+  margin: 20px;
 }
 
 .step-info {
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: center;
-	display: inline;
-	font-size: 14px;
-	color: var(--color-text-1);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  display: inline;
+  font-size: 14px;
+  color: var(--color-text-1);
 }
 
 .icon {
-	display: inline;
-	height: 16px;
-	width: 16px;
-	margin-right: 15px;
+  display: inline;
+  height: 16px;
+  width: 16px;
+  margin-right: 15px;
 }
 
 .grade-data {
-	color: var(--color-text-blue);
-	font-size: 14px;
-	font-weight: 400;
+  color: var(--color-text-blue);
+  font-size: 14px;
+  font-weight: 400;
+  margin: 12px 0 24px;
+}
+
+.grade-data > p {
+  margin-bottom: 4px;
 }
 
 .grade-data span {
-	font-weight: 500;
+  font-weight: 500;
+}
+
+.tables {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+
+@media (min-width: 640px) {
+  main {
+    margin: 40px;
+  }
+
+  .tables {
+    margin-top: 30px;
+  }
+}
+
+@media (min-width: 1024px) {
+  main {
+    margin: 40px 100px;
+  }
+  .grade-data {
+    font-size: 16px;
+  }
+
+  .tables {
+    justify-content: space-evenly;
+  }
 }
 </style>
