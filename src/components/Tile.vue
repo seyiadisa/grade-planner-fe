f90ba7f242f3e016e6f9dd6195d51487d3236a1f
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2024-02-10 04:14:34 +0100

    Make home page responsive

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,58 +1,74 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-	<div class="tile">
-		<div class="num-display">
-			<slot name="num"></slot>
-		</div>
-		<div class="icon">
-			<slot name="icon"></slot>
-		</div>
-		<p class="title">
-			<slot name="title"></slot>
-		</p>
-		<p class="description">
-			<slot name="description"></slot>
-		</p>
-	</div>
+  <div class="tile">
+    <div class="num-display">
+      <slot name="num"></slot>
+    </div>
+    <div class="icon">
+      <slot name="icon"></slot>
+    </div>
+    <p class="title">
+      <slot name="title"></slot>
+    </p>
+    <p class="description">
+      <slot name="description"></slot>
+    </p>
+  </div>
 </template>
 
 <style scoped>
 .tile {
-	border-radius: 8px;
-	padding: 20px 15px 20px 15px;
-	border: 1px solid var(--color-border);
-	background-color: var(--color-background-tile);
-	/* box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.25), 0px 0px 1px 0px rgba(0, 0, 0, 0.25); */
-	@apply flex flex-col w-1/2 items-center;
+  border-radius: 8px;
+  padding: 20px 15px 20px 15px;
+  border: 1px solid var(--color-border);
+  background-color: var(--color-background-tile);
+  /* box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.25), 0px 0px 1px 0px rgba(0, 0, 0, 0.25); */
+  @apply flex flex-col w-2/3 items-center;
 }
 
 .num-display {
-	align-self: flex-start;
-	font-size: 16px;
-	font-weight: 400;
-	color: var(--color-text-4);
+  align-self: flex-start;
+  font-size: 16px;
+  font-weight: 400;
+  color: var(--color-text-4);
 }
 
 .icon {
-	@apply my-4;
+  @apply my-4;
 }
 
 .title {
-	color: var(--color-text-2);
-	font-size: 18px;
-	font-weight: 600;
-	margin-bottom: 15px;
-	text-align: center;
+  color: var(--color-text-2);
+  font-size: 16px;
+  font-weight: 600;
+  margin-bottom: 15px;
+  text-align: center;
 }
 
 .description {
-	color: var(--color-text-1);
-	font-size: 16px;
-	font-weight: 500;
-	text-align: center;
-	/* @apply mx-4; */
+  color: var(--color-text-1);
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+  /* @apply mx-4; */
+}
+
+@media (min-width: 640px) {
+  .tile {
+    @apply w-1/2;
+  }
+  .title {
+    font-size: 18px;
+  }
+  .description {
+    font-size: 16px;
+  }
+}
+
+@media (min-width: 1024px) {
+  .tile {
+    height: 20em;
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,57 +7,69 @@ import IconArrow from "@/components/icons/IconArrowRight.vue";
 </script>
 
 <template>
-  <header>
-    <h1>AWE</h1>
-    <span>Start Measuring, Start Growing</span>
-    <p>Track your results growth as you estimate with [] in 3 simple steps</p>
-  </header>
+  <main>
+    <header>
+      <h1>AWE</h1>
+      <span>Start Measuring, Start Growing</span>
+      <p>Track your results growth as you estimate with [] in 3 simple steps</p>
+    </header>
 
-  <div class="tiles">
-    <Tile>
-      <template #icon>
-        <IconResult />
-      </template>
-      <template #num>01</template>
-      <template #title>Upload Result</template>
-      <template #description>Upload official/unofficial transcripts or manually enter results for past
-        semesters</template>
-    </Tile>
-    <Tile>
-      <template #icon>
-        <IconCourse />
-      </template>
-      <template #num>02</template>
-      <template #title>Upload Course Structure</template>
-      <template #description>Upload semester registrations for each semester or manually enter course details with grade
-        and
-        credit information</template>
-    </Tile>
-    <Tile>
-      <template #icon>
-        <IconEstimate />
-      </template>
-      <template #num>03</template>
-      <template #title>Calculate Estimate</template>
-      <template #description>Set a desired CGPA goal and see the estimated grades needed to achieve it</template>
-    </Tile>
-  </div>
+    <div class="tiles">
+      <Tile>
+        <template #icon>
+          <IconResult />
+        </template>
+        <template #num>01</template>
+        <template #title>Upload Result</template>
+        <template #description
+          >Upload official/unofficial transcripts or manually enter results for
+          past semesters
+        </template>
+      </Tile>
+      <Tile>
+        <template #icon>
+          <IconCourse />
+        </template>
+        <template #num>02</template>
+        <template #title>Upload Course Structure</template>
+        <template #description
+          >Upload semester registrations for each semester or manually enter
+          course details with grade and credit information</template
+        >
+      </Tile>
+      <Tile>
+        <template #icon>
+          <IconEstimate />
+        </template>
+        <template #num>03</template>
+        <template #title>Calculate Estimate</template>
+        <template #description
+          >Set a desired CGPA goal and see the estimated grades needed to
+          achieve it</template
+        >
+      </Tile>
+    </div>
 
-  <div class="btn-container">
-    <RouterLink to="/upload-result" class="link-button">
-      Start estimating
-      <IconArrow class="arrow" />
-    </RouterLink>
-  </div>
+    <div class="btn-container">
+      <RouterLink to="/upload-result" class="link-button">
+        Start estimating
+        <IconArrow class="arrow" />
+      </RouterLink>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+main {
+  margin: 20px;
+}
+
 header {
   text-align: center;
 }
 
 header span {
-  font-size: 36px;
+  font-size: 28px;
   font-weight: 700;
   background: var(--color-linear-gradient);
   background-clip: text;
@@ -68,7 +80,7 @@ header span {
 
 header p {
   color: var(--color-text-1);
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 500;
   @apply mx-7 my-4;
 }
@@ -88,11 +100,46 @@ header p {
   padding: 8px 15px;
   border-radius: 32px;
   color: #fff;
+  font-size: 14px;
 }
 
 .link-button .arrow {
   display: inline;
   margin-left: 4px;
 }
-</style>
 
+@media (min-width: 640px) {
+  main {
+    margin: 40px;
+  }
+
+  header {
+    margin-bottom: 60px;
+  }
+
+  header span {
+    font-size: 36px;
+  }
+
+  header p {
+    font-size: 16px;
+  }
+
+  .tiles {
+    @apply gap-y-12;
+  }
+}
+
+@media (min-width: 1024px) {
+  main {
+    margin: 60px;
+  }
+  header span {
+    font-size: 48px;
+  }
+  .tiles {
+    margin: 60px;
+    @apply flex-row gap-16;
+  }
+}
+</style>
